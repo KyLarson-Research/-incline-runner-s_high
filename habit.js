67d6cdd6a0d20f90747de3111f5c46550efd3ecc
Kyle Larson <kylel@email.arizona.edu>
2020-12-26 20:55:33 -0700

    updated pictures

--- habit.js
+++ habit.js
@@ -2,18 +2,18 @@
 //Author: Kyle Carlton Larson 12-1-2020
 //Purpose: to provide functionality to the qsr project
 const headerdiv = document.getElementById('headerDiv');
-document.getElementById('headerImg').src = "IMG-0344.JPG";
+document.getElementById('headerImg').src = "20170702_080422.JPG";
 var j = 0;
 number_of_images =3;
 function changeImage() {
 	if(j%number_of_images==0){
-		document.getElementById('headerImg').src = "IMG-0140.JPG";
+		document.getElementById('headerImg').src = "20160403_075821.JPG";
 	}
 	else if(j%number_of_images==1){
-		document.getElementById('headerImg').src = "IMG-0344.JPG";
+		document.getElementById('headerImg').src = "20160416_064344.JPG";
 	}
 	else{
-		document.getElementById('headerImg').src = "IMG-0437.JPG";
+		document.getElementById('headerImg').src = "20170702_080422.JPG";
 	}
 	j++;
 	}
